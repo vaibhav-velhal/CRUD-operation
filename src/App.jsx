562276deba5a89dc50dd8bf8f9143175f70d8e00
vaibhav-velhal <vaibhav.velhal1614@gmail.com>
2026-03-20 16:38:05 +0530

    Implement routing and GET users from JSON Server

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,19 @@
 import './App.css'
+import { Routes, Route, NavLink } from 'react-router-dom'
+import UserList from './UserList'
 
 function App() {
 
   return (
-    <>
+    <div>
+      <nav>
+        <NavLink to="/">UserList</NavLink>
+      </nav>
 
-    </>
+      <Routes>
+        <Route path='/' element={<UserList />} />
+      </Routes>
+    </div>
   )
 }
 
---
+++ src/UserList.jsx
@@ -0,0 +1,48 @@
+import { useState, useEffect } from "react";
+
+export default function UserList() {
+    const url = "http://localhost:3000/users";
+
+    const [userData, setUserData] = useState([]);
+
+    useEffect(() => {
+        getUserData();
+    }, [])
+
+    const getUserData = async () => {
+        let response = await fetch(url);
+        response = await response.json();
+        console.log(response)
+        setUserData(response);
+    }
+
+    return (
+        <div>
+            <h1>UserList</h1>
+
+            <div className="table-container">
+                <table>
+                    <thead>
+                        <tr>
+                            <th>Name</th>
+                            <th>Age</th>
+                            <th>Email</th>
+                        </tr>
+                    </thead>
+
+                    <tbody>
+                        {
+                            userData.map((user) => (
+                                <tr key={user.name}>
+                                    <td>{user.name}</td>
+                                    <td>{user.age}</td>
+                                    <td>{user.email}</td>
+                                </tr>
+                            ))
+                        }
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    )
+}
